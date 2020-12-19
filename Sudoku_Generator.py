6ecff7cba2366382be4f7e53cb23381df38d851d
lordquinnington <73671219+lordquinnington@users.noreply.github.com>
2020-12-19 15:58:36 +0000

    Add files via upload

--- Sudoku_Generator.py
+++ Sudoku_Generator.py
@@ -1,33 +1,22 @@
 #~~~~~ Sudoku Generator ~~~~~#
 
 import random
-#from Sudoku_Solver import formatSudokuGridTo5D
 
 ################################################ generating functions ################################################
 
 def generateCompletedGrid():
-##    newGrid, squareSize = formatSudokuGridTo5D("Blank")
-##    print(newGrid)#
-##    rowNums = []
-##    for i in range(3):
-##        for j in range(3):
-##            newGrid = eliminateNotPossibleNums(newGrid,3,0,i,0,j)
-##            try:
-##                newGrid[0][i][0][j].remove(0)
-##                choiceToFillIn = random.choice(newGrid[0][i][0][j])
-##                newGrid[0][i][0][j].clear()
-##                newGrid[0][i][0][j].append(choiceToFillIn)
-##            except ValueError:
-##                pass
-##            rowNums.append(newGrid[0][i][0][j][0])
-##    newGrid.clear()
+    newGrid = []
+    # generate the initial random row
     rowNums = []
-    possibilities = [1,2,3,4,5,6,7,8,9]
+    options = [1,2,3,4,5,6,7,8,9]
     for i in range(9):
-        choiceToFillIn = 
+        rowNums.append(random.choice(options))
+        options.remove(rowNums[i])
+    # generate the shifted grid
     for i in range(3):
+        print("---------------------------")#
         for j in range(3):
-            if i != 0: #and j == 0:
+            if i != 0:
                 if j == 0:
                     rowNums = rowNums[8:]+rowNums[:8]
                     print(rowNums)#
@@ -36,15 +25,53 @@ def generateCompletedGrid():
                     print(rowNums)#
             else:
                 rowNums = rowNums[6:]+rowNums[:6]
-                print(rowNums)
+                print(rowNums)#
             newGrid.append(rowNums)
-        
+    print('\n')#
+    # randomly rearrange the big rows
+    bigRows = []
+    for i in range(3):
+        tempArray1 = []
+        for j in range(3):
+            tempArray1.append(newGrid[3*i+j])
+            print("ta1>",tempArray1[j])#
+        bigRows.append(tempArray1)
+    print("br> ",bigRows)#
+    newBigRowOrder = []
+    options = [0,1,2]
+    for i in range(3):
+        x = random.choice(options)
+        newBigRowOrder.append(bigRows[x])
+        print("brx>",bigRows[x])#
+        options.remove(x)
+    print("nbro>",newBigRowOrder)#
+    print("\n big rows>")#
+    for i in range(3):#
+        print("---------------------------")#
+        for j in range(3):#
+            print(newBigRowOrder[i][j])#
+    # randomly rearrange the small rows inside the big rows
+    newSmallRowOrder = []
+    for i in range(3):
+        tempArray1 = []
+        options = [0,1,2]
+        for j in range(3):
+            x = random.choice(options)
+            tempArray1.append(newBigRowOrder[i][x])
+            options.remove(x)
+        newSmallRowOrder.append(tempArray1)
+    print("\n small rows>")#
+    for i in range(3):#
+        print("---------------------------")#
+        for j in range(3):#
+            print(newSmallRowOrder[i][j])#
+            
     
     return newGrid
         
         
 newGrid = generateCompletedGrid()
-print(newGrid)
+print('\n',newGrid)
 
 '''perhaps generate some random numbers and then fill in the rest from randomly choosing the least possible numbers (or both, generate 1 and then fill some in and generate again (but that might be slower))'''
 '''need to check for possible invalid inputs when randomly filling in so perhaps utilise the find numbers to remove function?'''
