81538f6bb6d5ff0e43eb481b41002eebdb97ef20
lordquinnington <73671219+lordquinnington@users.noreply.github.com>
2020-12-19 21:30:12 +0000

    Add files via upload

--- Sudoku_Main.py
+++ Sudoku_Main.py
@@ -4,7 +4,7 @@
 
 validInput = False
 while not validInput:
-    option = input("pick an option: \n1) solve a grid \n2) generate a 9x9 grid \noption >")
+    option = input("pick an option: \n1) solve a grid \n2) generate a grid \noption >")
     if option == '1':
         from Sudoku_Solver import initialiseSolving
         initialiseSolving()
