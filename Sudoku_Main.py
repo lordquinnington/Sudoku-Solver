1ad55e00ee040a12df58a67ce52753b3c5fc4a16
lordquinnington <73671219+lordquinnington@users.noreply.github.com>
2020-12-20 19:34:32 +0000

    Add files via upload

--- Sudoku_Main.py
+++ Sudoku_Main.py
@@ -3,10 +3,10 @@
 ################################################ main program ################################################
 
 validInput = False
-while not validInput:
-    option = input("pick an option: \n1) solve a grid \n2) generate a complete grid \noption >")
+while not validInput:    # ensures the user enters in a valid option
+    option = input("pick an option: \n1) solve a grid \n2) generate a complete grid \n3) create a puzzle \noption >")    # gets the user to choose which option they would like
     if option == '1':
-        from Sudoku_Solver import initialiseSolving
+        from Sudoku_Solver import initialiseSolving    # imports the functions the user requires
         initialiseSolving()
         validInput = True
     elif option == '2':
@@ -21,5 +21,5 @@ while not validInput:
         print("enter a valid option")
         validInput = False
 
-print("danke for doing sudoku-y stuff")
+print("danke for doing sudoku-y stuff")    # mandatory gratitude giving
     
