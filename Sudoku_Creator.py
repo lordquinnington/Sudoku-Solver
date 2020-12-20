defbaae0df3033e2b251b4cfaebf76bf5c2cedbe
lordquinnington <73671219+lordquinnington@users.noreply.github.com>
2020-12-20 18:08:30 +0000

    Add files via upload

--- Sudoku_Creator.py
+++ Sudoku_Creator.py
@@ -6,7 +6,7 @@ from Sudoku_Solver import formatSudokuGridTo5DFrom2D, print5DSudokuGrid, formatS
 
 ################################################ formatting functions ################################################
 
-def writeNewGridToCSV(gridArray2D,gridNumber):
+def writeNewGridToCSV(gridArray2D,gridNumber):    # function to write the new grid to a file 
     with open("Sudoku_Grid_New_"+gridNumber+".csv","w",newline='') as completedGridFile:
         toWrite = csv.writer(completedGridFile,delimiter=',')
         for row in gridArray2D:
@@ -15,23 +15,23 @@ def writeNewGridToCSV(gridArray2D,gridNumber):
 
 ################################################ creating functions ################################################
 
-def removeRandomNumber(gridArray):
+def removeRandomNumber(gridArray):    # function to remove a random number from the grid
     zero = True
     while zero:
         options = [0,1,2]
-        a = random.choice(options)
+        a = random.choice(options)    # generates 4 random coordinates to remove from
         b = random.choice(options)
         c = random.choice(options)
         d = random.choice(options)
-        if gridArray[a][b][c][d][0] != 0:
+        if gridArray[a][b][c][d][0] != 0:    # only removes it if the square is filled in
             gridArray[a][b][c][d][0] = 0
             zero = False
-        else:
+        else:    # if the square is 0, it will have to generate another set of random coordinates
             zero = True
     return gridArray
     
-def createNewPuzzle(newGrid,difficulty):
-    squaresToKeep = random.randint((25+5*difficulty),((25+5*difficulty)+5))
+def createNewPuzzle(newGrid,difficulty):    # function to remove the right amount of numbers from the grid
+    squaresToKeep = random.randint((25+5*difficulty),((25+5*difficulty)+5))    # decides how many numbers to keep - 40-45 easy, 35-40 medium, 30-35 hard, 25-30 expert
     for i in range(81-squaresToKeep):
         newGrid = removeRandomNumber(newGrid)
     return newGrid
@@ -39,15 +39,15 @@ def createNewPuzzle(newGrid,difficulty):
 def initialiseCreating():
     gridNumber = input("enter a grid number >")
     validInput = False
-    while not validInput:
+    while not validInput:    # ensures the user sets the difficulty correctly
         try:
-            difficulty = int(input("enter a difficulty: \n1) expert \n2) hard \n3) medium \n4) easy \noption>"))
+            difficulty = int(input("enter a difficulty: \n1) expert \n2) hard \n3) medium \n4) easy \noption>"))    # user sets the difficulty they would like
             if difficulty >= 1 and difficulty <= 4:
                 validInput = True
-            else:
+            else:    # the number entered is out of range of the valid inputs
                 print("enter a valid input")
                 validInput = False
-        except ValueError:
+        except ValueError:    # the input is not an integer
             print("enter a valid option")
             validInput = False
     completedGrid = formatSudokuGridTo5DFrom2D(generateCompletedGrid(),3)
@@ -55,5 +55,5 @@ def initialiseCreating():
     newPuzzle = createNewPuzzle(completedGrid,difficulty-1)
     finishTime = time.time()
     print("new grid created (in "+str(round(finishTime-startTime,3))+"s):")
-    print5DSudokuGrid(newPuzzle,3)
-    writeNewGridToCSV(formatSudokuGridTo2DFrom5D(newPuzzle,3),gridNumber)
+    print5DSudokuGrid(newPuzzle,3)    # prints out the grid
+    writeNewGridToCSV(formatSudokuGridTo2DFrom5D(newPuzzle,3),gridNumber)    # writes the grid to a file
