3892ee51a830734671e512755b5c253d4aae033d
lordquinnington <73671219+lordquinnington@users.noreply.github.com>
2020-12-21 19:15:37 +0000

    Add files via upload

--- Sudoku_GUI.py
+++ Sudoku_GUI.py
@@ -3,17 +3,17 @@
 import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((1000,600))
+gameDisplay = pygame.display.set_mode((1000,601))
 pygame.display.set_caption("Sudoku Puzzle GUI")
-backgroundColour = (255,255,255)
-mainButtonColour = (255,255,255)
+white = (255,255,255)    # use for backgroundColour, mainButtonColour and squareColour
+black = (28,28,28)
 mainButtonColourHover = (200,200,200)
-squareColour = (255,255,255)
 squareColourHover = (175,195,200)
 squareColourPressed = (145,185,200)
 RCSColourSelected = (180,180,180)    # RCS = row/column/square
 otherButtonColour = (65,140,220)    # for the new games buttons etc
 otherButtonColourHover = (75,135,200)
+smallLineColour = (170,170,170)
 clock = pygame.time.Clock()
 finished = False
 
@@ -22,7 +22,15 @@ while not finished:
         if event.type == pygame.QUIT:
             finished = True
 
-    gameDisplay.fill(backgroundColour)
+    gameDisplay.fill(white)
+    for j in range(8):
+        #if j == 2 or j == 5:
+        #    pygame.draw.line(gameDisplay,black,start_pos=(60*j+90,30),end_pos=(60*j+90,571),width=2)
+        #    pygame.draw.line(gameDisplay,black,start_pos=(30,60*j+90),end_pos=(571,60*j+90),width=2)
+        if j != 2 or j != 5:
+            pygame.draw.line(gameDisplay,smallLineColour,start_pos=(60*j+90,30),end_pos=(60*j+90,571),width=2)
+            pygame.draw.line(gameDisplay,smallLineColour,start_pos=(30,60*j+90),end_pos=(571,60*j+90),width=2)
+    pygame.draw.rect(gameDisplay,black,(30,30,541,541),width=2)    # (dist. from left edge,dist. from top edge,width,height)
     pygame.display.flip()
     clock.tick(60)
 
