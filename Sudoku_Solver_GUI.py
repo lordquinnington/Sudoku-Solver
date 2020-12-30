3644077f316025c30700c1ca766b405e38379c56
lordquinnington <73671219+lordquinnington@users.noreply.github.com>
2020-12-30 13:11:09 +0000

    Add files via upload

--- Sudoku_Solver_GUI.py
+++ Sudoku_Solver_GUI.py
@@ -37,6 +37,8 @@ def runMainProgramGUI():
     black = (20,20,20)
     smallLineColour = (170,170,170)
     lightGrey = (225,225,225)
+    errorBoxColour = (242,242,242)
+    errorBoxOutlineColour = (255,135,135)
     squareColourHover = (220,250,255)
     squareColourPressed = (150,220,240)
     keypadColour = (170,170,170)
@@ -51,6 +53,8 @@ def runMainProgramGUI():
     keypadTextFont = pygame.font.SysFont('arial',30)
     solveButtonTextFont = pygame.font.SysFont('arial',40)
     otherTextFont = pygame.font.SysFont('lucidasansregular',22)
+    errorBoxFont1 = pygame.font.SysFont('arial',70)
+    errorBoxFont2 = pygame.font.SysFont('arial',25)
 
     timeTaken = "N/A"
     savedAs = "N/A"
@@ -109,6 +113,8 @@ def runMainProgramGUI():
                     restartGrid = True
                 if event.key == pygame.K_s:
                     solveGrid = True
+                if event.key == pygame.K_RETURN:
+                    notPossible = False
                 if event.key == pygame.K_UP:
                     moveSquareUp = True
                 if event.key == pygame.K_DOWN:
@@ -147,7 +153,7 @@ def runMainProgramGUI():
             for j in range(3):
                 if (113*i+621) < mousePos[0] < (113*i+734) and (96*j+30) < mousePos[1] < (96*j+126):    # for hovering (here so doesn't interfere with the other lines)
                     pygame.draw.rect(gameDisplay,lightGrey,(113*i+621,96*j+30,113,96))
-                    if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.type == pygame.MOUSEBUTTONDOWN and notPossible == False:
                         pygame.draw.rect(gameDisplay,keypadColourPressed,(113*i+621,96*j+30,113,96))
                         needToFillIn = True
                         numPressed = keypad[j][i]    # if the keypad button is pressed, the number (from an array in the same format) is recorded
@@ -155,7 +161,7 @@ def runMainProgramGUI():
         for i in range(2):
             if (170*i+621) < mousePos[0] < (170*i+791) and (318) < mousePos[1] < (384):
                 pygame.draw.rect(gameDisplay,lightGrey,(170*i+621,318,170,66))
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN and notPossible == False:
                     pygame.draw.rect(gameDisplay,keypadColourPressed,(170*i+621,318,170,66))
                     if i == 0:
                         needToErase = True
@@ -179,7 +185,7 @@ def runMainProgramGUI():
         if solved == False:
             if 671 < mousePos[0] < 911 and 409 < mousePos[1] < 474:
                 pygame.draw.rect(gameDisplay,otherButtonColourHover,(671,409,240,66),border_radius=8)
-                if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN and notPossible == False:
                     pygame.draw.rect(gameDisplay,otherButtonColourPressed,(671,409,240,66),border_radius=8)
                     solveGrid = True
             gameDisplay.blit(solveButtonTextFont.render("Solve",False,black),(745,419))
@@ -207,7 +213,7 @@ def runMainProgramGUI():
             for j in range(9):
                 if (60*i+32) < mousePos[0] < (60*i+90) and (60*j+32) < mousePos[1] < (60*j+90):
                     pygame.draw.rect(gameDisplay,squareColourHover,(60*i+32,60*j+32,58,58))    # hovering over the grid square
-                    if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.type == pygame.MOUSEBUTTONDOWN and notPossible == False:
                         squareClicked = True
                         coords = [(60*i+32),(60*j+32)]    # if the grid square is clicked on, its coordinates are marked
 
@@ -222,7 +228,7 @@ def runMainProgramGUI():
                         gridNumbers = gridNumbersFont.render(str(userGrid[j][i]),False,black)
                     gameDisplay.blit(gridNumbers,(60*i+48,60*j+34))
 
-        if restartGrid == True:
+        if restartGrid == True and notPossible == False:
             if userGrid != 0:
                 userGrid = 0     # when it loops back around again, the if statement at the start will make a new grid so don't need to initialise it back to a blank array here
                 solvedGrid = 0
@@ -231,17 +237,17 @@ def runMainProgramGUI():
             savedAs = "N/A"
             restartGrid = False
 
-        if needToErase == True:
+        if needToErase == True and notPossible == False:
             if coords != 0 and userGrid != 0 and solved == False:
                 userGrid[int((coords[1]-32)/60)][int((coords[0]-32)/60)] = 0
             needToErase = False
 
-        if needToFillIn == True:
+        if needToFillIn == True and notPossible == False:
             if coords != 0 and userGrid != 0 and solved == False:
                 userGrid[int((coords[1]-32)/60)][int((coords[0]-32)/60)] = numPressed
             needToFillIn = False
 
-        if solveGrid == True:
+        if solveGrid == True and notPossible == False:
             if userGrid != 0 and solved == False:
                 tempGrid = formatSudokuGridTo5DFrom2D(userGrid,3)
                 startTime = time.time()
@@ -259,24 +265,38 @@ def runMainProgramGUI():
             solveGrid = False
 
         if moveSquareUp == True:
-            if coords != 0 and 0 < int((coords[1]-32)/60) <= 8:
+            if coords != 0 and 0 < int((coords[1]-32)/60) <= 8 and notPossible == False:
                 coords[1] = coords[1] - 60
             moveSquareUp = False
 
         if moveSquareDown == True:
-            if coords != 0 and 0 <= int((coords[1]-32)/60) < 8:
+            if coords != 0 and 0 <= int((coords[1]-32)/60) < 8 and notPossible == False:
                 coords[1] = coords[1] + 60
             moveSquareDown = False
 
         if moveSquareLeft == True:
-            if coords != 0 and 0 < int((coords[0]-32)/60) <= 8:
+            if coords != 0 and 0 < int((coords[0]-32)/60) <= 8 and notPossible == False:
                 coords[0] = coords[0] - 60
             moveSquareLeft = False
 
         if moveSquareRight == True:
-            if coords != 0 and 0 <= int((coords[0]-32)/60) < 8:
+            if coords != 0 and 0 <= int((coords[0]-32)/60) < 8 and notPossible == False:
                 coords[0] = coords[0] + 60
             moveSquareRight = False
+
+        if notPossible == True:
+            pygame.draw.rect(gameDisplay,errorBoxColour,(300,200,400,200),border_radius=10)
+            pygame.draw.rect(gameDisplay,errorBoxOutlineColour,(298,198,404,204),border_radius=10,width=2)
+            gameDisplay.blit(errorBoxFont1.render("Oh dear...",False,black),(377,207))
+            gameDisplay.blit(errorBoxFont2.render("This grid doesn't appear to be solvable.",False,black),(323,287))
+            gameDisplay.blit(errorBoxFont2.render("Please change it :)",False,black),(415,317))
+            pygame.draw.rect(gameDisplay,otherButtonColour,(438,350,120,43),border_radius=4)
+            if 438 < mousePos[0] < 558 and 350 < mousePos[1] < 393:
+                pygame.draw.rect(gameDisplay,otherButtonColourHover,(438,350,120,43),border_radius=4)
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pygame.draw.rect(gameDisplay,otherButtonColourPressed,(438,350,120,43),border_radius=4)
+                    notPossible = False
+            gameDisplay.blit(errorBoxFont2.render("Will do!",False,black),(465,357))
 
         pygame.display.flip()
         time.sleep(0.082)
